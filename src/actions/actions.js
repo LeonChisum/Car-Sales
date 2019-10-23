95afac450129e940f843c6d7a8e8c7a5af517e6f
Leon Chisum <leonchisum3306@gmail.com>
2019-10-23 18:09:22 -0400

    store imported with reducer

---
+++ src/actions/actions.js
@@ -0,0 +1,11 @@
+export const addFeature = () => {
+    return {
+        type: "ADD_FEATURE"
+    }
+}
+
+export const removeFeature = () => {
+    return {
+        type: "REMOVE_FEATURE"
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createScore} from 'redux';
+import {Provider} from 'react-redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// Store
+const store = createScore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+                <Provider store ={store}>
+                    <App />
+                </Provider>, rootElement);
